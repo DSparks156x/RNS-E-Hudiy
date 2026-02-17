d827f720d318acbaf40b5af33598f39927f540d0
Drew Sparks <drew.sparks156@gmail.com>
2026-02-16 20:37:14 -0700

    comment

--- dis_client/icons.py
+++ dis_client/icons.py
@@ -33,7 +33,7 @@ ICONS = {
 }
 
 # =============================================================================
-# 31×37 FULL NAVIGATION ICON PACK — ALL 18 MANEUVERS (HQ Edition)
+# 31×37 FULL NAVIGATION ICON PACK — ALL 44 MANEUVERS stolen from google edition
 # =============================================================================
 
 # Import all new icons and the BITMAPS dictionary
